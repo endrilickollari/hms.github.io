62c0b6f0304a26e68c8ae2b16d392bf5df76772d
endrilickollari <32954585+endrilickollari@users.noreply.github.com>
2019-01-05 18:51:59 +0100

    Add files via upload

---
+++ hms/hmsdb.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 31, 2017 at 03:15 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hospital1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointmenttb`
+--
+
+CREATE TABLE `appointmenttb` (
+  `fname` varchar(80) NOT NULL,
+  `lname` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `contact` varchar(80) NOT NULL,
+  `doctor` varchar(80) NOT NULL,
+  `payment` varchar(40) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctb`
+--
+
+CREATE TABLE `doctb` (
+  `name` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctb`
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logintb`
+--
+
+CREATE TABLE `logintb` (
+  `username` varchar(80) NOT NULL,
+  `password` varchar(80) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `logintb`
+--
+
+INSERT INTO `logintb` (`username`, `password`) VALUES
+('admin', 'admin123');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
